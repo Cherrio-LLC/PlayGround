2a620803fe70c1806c9e97439489fdad039c4233
Cherrio-LLC <cherrio.llc@gmail.com>
2021-06-24 10:42:35 +0100

    Initial commit

---
+++ app/src/main/java/com/cherrio/playground/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.cherrio.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
